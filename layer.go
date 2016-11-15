f43060d365b6578157414bf33e9e2a95e8e97966
binaryplease <binaryplease@gmail.com>
2016-11-15 15:55:08 +0100

    added structs for other layer types

--- layer.go
+++ layer.go
@@ -1,10 +1,45 @@
 package robonet
 
 //ConvLayer basic type for a convolutional layer
+//The layer will compute the output of neurons that are connected to local regions in the input,
+//each computing a dot product between their weights and a small region they are connected to in
+//the input volume. This may result in volume such as [32x32x12] if we decided to use 12 filters.
 type ConvLayer struct {
+	Layer
 	kernels  []Kernel
 	stridesR []int
 	stridesC []int
+}
+
+//PoolLayer will perform a downsampling operation along the spatial dimensions (width, height), resulting in volume such as [16x16x12].
+type PoolLayer struct {
+	Layer
+	//TODO
+}
+
+//NormLayer is a normalisation layer
+type NormLayer struct {
+	Layer
+	//TODO
+}
+
+//FCLayer (i.e. fully-connected) layer will compute the class scores, resulting in volume of size [1x1x10], where each of the 10 numbers correspond to a class score, such as among the 10 categories of CIFAR-10. As with ordinary Neural Networks and as the name implies, each neuron in this layer will be connected to all the numbers in the previous volume.
+type FCLayer struct {
+	Layer
+	//TODO
+}
+
+//InputLayer [32x32x3] will hold the raw pixel values of the image, in this case an image of width 32, height 32, and with three color channels R,G,B.
+type InputLayer struct {
+	Layer
+	//TODO
+}
+
+//ReluLayer will apply an elementwise activation function, such as the max(0,x)max(0,x)
+//thresholding at zero. This leaves the size of the volume unchanged ([32x32x12]).
+type ReluLayer struct {
+	Layer
+	//TODO
 }
 
 //AddKernel adds a kernel to a layer
