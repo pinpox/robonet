078f38c649960d31ae7667d7c51e9145edfb1d06
binaryplease <binaryplease@gmail.com>
2016-11-18 14:34:30 +0100

    added test cases for sigmoidfast

--- utils_test.go
+++ utils_test.go
@@ -2,13 +2,6 @@ package robonet
 
 import "testing"
 
-func TestSigmoidFast(t *testing.T) {
-
-	if SigmoidFast(4) != 0.8 {
-		t.Error("Expected ", 0.8, ", got ", SigmoidFast(4))
-	}
-}
-
 func TestEqual3Dim(t *testing.T) {
 	if !Equal3Dim(1, 2, 3, 1, 2, 3) {
 		t.Error("Expected true, got", Equal3Dim(1, 2, 3, 1, 2, 3))
@@ -27,5 +20,26 @@ func TestOdd3Dim(t *testing.T) {
 	}
 	if !Odd3Dim(2, 1, 1) {
 		t.Error("Expected false, got", Odd3Dim(2, 1, 1))
+	}
+}
+
+func TestSigmoidFast(t *testing.T) {
+	type args struct {
+		x float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want float64
+	}{
+		//TODO add more test cases
+		{"Test for zero", args{0.0}, 0.0},
+		{"Test for zero", args{4}, 0.8}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SigmoidFast(tt.args.x); got != tt.want {
+				t.Errorf("SigmoidFast() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
