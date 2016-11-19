bc803202758d3427573969637928fb04a96be6a6
binaryplease <binaryplease@gmail.com>
2016-11-19 02:15:25 +0100

    fixed #21

--- conv_layer_test.go
+++ conv_layer_test.go
@@ -6,33 +6,37 @@ import (
 )
 
 func TestConvLayer_AddKernel(t *testing.T) {
-	type fields struct {
-		LayerFields LayerFields
-		kernels     []Kernel
-		stridesR    []int
-		stridesC    []int
-	}
-	type args struct {
-		kern    Kernel
-		strideR int
-		strideC int
-	}
+
+	lay1 := *new(ConvLayer)
+	lay2 := *new(ConvLayer)
+
+	ker := NewKernelRandom(3, 3, 3)
+
+	lay1.AddKernel(ker, 2, 2)
+	lay1.AddKernel(ker, 2, 2)
+	lay1.AddKernel(ker, 2, 2)
+
+	lay2.AddKernel(ker, 2, 2)
+	lay2.AddKernel(ker, 2, 2)
+	lay2.AddKernel(ker, 2, 2)
+	lay2.AddKernel(ker, 2, 2)
+	lay2.AddKernel(ker, 2, 2)
+	lay2.AddKernel(ker, 2, 2)
+
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name string
+		want int
+		lay  ConvLayer
 	}{
-	// TODO: Add test cases.
-	}
+		{"3 Kernels", 3, lay1},
+		{"6 kernels", 6, lay2}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := &ConvLayer{
-				LayerFields: tt.fields.LayerFields,
-				kernels:     tt.fields.kernels,
-				stridesR:    tt.fields.stridesR,
-				stridesC:    tt.fields.stridesC,
+			l := tt.lay
+
+			if got := len(l.Kernels()); got != tt.want {
+				t.Errorf("ConvLayer.AddKernel() = %v, want %v", got, tt.want)
 			}
-			l.AddKernel(tt.args.kern, tt.args.strideR, tt.args.strideC)
 		})
 	}
 }
