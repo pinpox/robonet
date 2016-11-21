8338b2f717c5c24f5a640b99cd7e957fe0843fea
binaryplease <binaryplease@gmail.com>
2016-11-21 13:01:41 +0100

    implemented pooling layer

--- pool_layer.go
+++ pool_layer.go
@@ -1,12 +1,61 @@
 package robonet
 
+import (
+	"errors"
+	"log"
+)
+
 //PoolLayer will perform a downsampling operation along the spatial dimensions (width, height), resulting in volume such as [16x16x12].
 type PoolLayer struct {
 	LayerFields
-	//TODO
+	SizeR   int
+	SizeC   int
+	StrideR int
+	StrideC int
 }
 
-//Calculate for Pooling layers
+//Calculate for Pooling layers applies the pooling operation after the parameters have been set.
 func (lay *PoolLayer) Calculate() {
-	//TODO
+
+	if lay.SizeR == 0 || lay.SizeC == 0 || lay.StrideR == 0 || lay.StrideC == 0 {
+		log.Fatal(errors.New("PoolLayer: Parameters not set"))
+	}
+
+	if lay.input.Rows()%lay.StrideR != 0 || lay.input.Collumns()%lay.StrideC != 0 {
+		log.Fatal(errors.New("PoolLayer: Input Size not divisible by factor"))
+	}
+
+	lay.output = NewVolume(lay.input.Rows()/lay.StrideR, lay.input.Collumns()/lay.StrideC, lay.input.Depth())
+
+	for r := 0; r < lay.input.Rows(); r++ {
+		for c := 0; c < lay.input.Collumns(); c++ {
+
+			res := maxPool(lay.input.SubVolumePadded(r*lay.StrideR, c*lay.StrideC, lay.SizeR, lay.SizeC))
+
+			for d := 0; d < lay.input.Depth(); d++ {
+
+				lay.output.SetAt(r, c, d, res[d])
+			}
+		}
+	}
+}
+
+//maxPool calculates the maximums of a volume's depths and returns them as slice.
+//The position in the slice matches the depth of that value in the input volume
+func maxPool(vol Volume) (res []float64) {
+
+	for d := 0; d < vol.Depth(); d++ {
+
+		max := 0.0
+
+		for r := 0; r < vol.Rows(); r++ {
+			for c := 0; c < vol.Collumns(); c++ {
+				if tmp := vol.GetAt(r, c, d); tmp > max {
+					max = tmp
+				}
+			}
+		}
+		res = append(res, max)
+	}
+	return res
 }
--- pool_layer_test.go
+++ pool_layer_test.go
@@ -1,6 +1,9 @@
 package robonet
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestPoolLayer_Calculate(t *testing.T) {
 	type fields struct {
@@ -18,6 +21,26 @@ func TestPoolLayer_Calculate(t *testing.T) {
 				LayerFields: tt.fields.LayerFields,
 			}
 			lay.Calculate()
+		})
+	}
+}
+
+func Test_maxPool(t *testing.T) {
+	type args struct {
+		vol Volume
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantRes []float64
+	}{
+	// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRes := maxPool(tt.args.vol); !reflect.DeepEqual(gotRes, tt.wantRes) {
+				t.Errorf("maxPool() = %v, want %v", gotRes, tt.wantRes)
+			}
 		})
 	}
 }
