1c4167d5a61212950996e533bcf9371a3217a77c
binaryplease <binaryplease@gmail.com>
2016-11-23 19:31:16 +0100

    removed NVOlume from tests

--- volume.go
+++ volume.go
@@ -3,6 +3,7 @@ package robonet
 import (
 	"errors"
 	"fmt"
+	//"github.com/Kunde21/numgo"
 	"github.com/gonum/matrix/mat64"
 	"log"
 	"math"
@@ -37,13 +38,13 @@ type Volume interface {
 	SubVolumePadded(int, int, int, int) Volume
 }
 
-// NVolume implements Volume
-type NVolume struct {
+// D3Volume implements Volume
+type D3Volume struct {
 	Fields []mat64.Dense
 }
 
 //SetAll sets all values of the volume from another equal-sized volume
-func (vol *NVolume) SetAll(v Volume) {
+func (vol *D3Volume) SetAll(v Volume) {
 
 	if v == nil {
 		panic("tried to set Volume with nil input")
@@ -51,15 +52,15 @@ func (vol *NVolume) SetAll(v Volume) {
 
 	if !EqualVolDim(vol, v) {
 		fmt.Printf("set %vx%vx%v with %vx%vx%v", vol.Rows(), vol.Collumns(), vol.Depth(), v.Rows(), v.Collumns(), v.Depth())
-		log.Fatal(errors.New("NVolumedimensions do not match"))
+		log.Fatal(errors.New("D3Volumedimensions do not match"))
 	}
 
-	*vol = *v.(*NVolume)
+	*vol = *v.(*D3Volume)
 
 }
 
-//Shape returns the extend in every dimension of a NVolume
-func (vol *NVolume) Shape() (int, int, int) {
+//Shape returns the extend in every dimension of a D3Volume
+func (vol *D3Volume) Shape() (int, int, int) {
 	d := len(vol.Fields)
 	if d != 0 {
 		r, c := vol.Fields[0].Dims()
@@ -68,8 +69,8 @@ func (vol *NVolume) Shape() (int, int, int) {
 	return 0, 0, 0
 }
 
-//Apply applys the given kernel to the whole volume, returnung a NVolume with 1 depth
-func (vol *NVolume) Apply(kern Kernel, strideR, strideC int) {
+//Apply applys the given kernel to the whole volume, returnung a D3Volume with 1 depth
+func (vol *D3Volume) Apply(kern Kernel, strideR, strideC int) {
 
 	r, c, _ := vol.Shape()
 	r2, c2, _ := kern.Shape()
@@ -91,7 +92,7 @@ func (vol *NVolume) Apply(kern Kernel, strideR, strideC int) {
 }
 
 //Norm normalizes the volume to a given maximum and 0
-func (vol *NVolume) Norm(max float64) {
+func (vol *D3Volume) Norm(max float64) {
 
 	volmin := vol.Min()
 
@@ -115,9 +116,9 @@ func (vol *NVolume) Norm(max float64) {
 	}
 }
 
-//New generates a NVolume of fixed size filled with zeros
-func New(r, c, d int) *NVolume {
-	v := new(NVolume)
+//New generates a D3Volume of fixed size filled with zeros
+func New(r, c, d int) *D3Volume {
+	v := new(D3Volume)
 	v.Fields = []mat64.Dense{}
 
 	for i := 0; i < d; i++ {
@@ -127,8 +128,8 @@ func New(r, c, d int) *NVolume {
 	return v
 }
 
-//NewWithData generates a NVolume of fixed size filled with custom data
-func NewWithData(r, c, d int, data []float64) *NVolume {
+//NewWithData generates a D3Volume of fixed size filled with custom data
+func NewWithData(r, c, d int, data []float64) *D3Volume {
 
 	if len(data) != r*c*d {
 		fmt.Printf("supplied %v data items for %v length", len(data), r*c*d)
@@ -147,8 +148,8 @@ func NewWithData(r, c, d int, data []float64) *NVolume {
 	return v
 }
 
-//NewRand generates a NVolume of fixed size filled with values between 0 and 1
-func NewRand(r, c, d int) *NVolume {
+//NewRand generates a D3Volume of fixed size filled with values between 0 and 1
+func NewRand(r, c, d int) *D3Volume {
 	data := []float64{}
 
 	for i := 0; i < r*c*d; i++ {
@@ -158,7 +159,7 @@ func NewRand(r, c, d int) *NVolume {
 	return NewWithData(r, c, d, data)
 }
 
-//NewFull generates a NVolume of fixed size filled with values between 0 and 1
+//NewFull generates a D3Volume of fixed size filled with values between 0 and 1
 func NewFull(r, c, d int, fil float64) Volume {
 
 	data := []float64{}
@@ -170,9 +171,9 @@ func NewFull(r, c, d int, fil float64) Volume {
 	return NewWithData(r, c, d, data)
 }
 
-//SubVolumePadded returns a part of the original NVolume. cR and cC determine the center of copying, r and c the size of the subvolume.
+//SubVolumePadded returns a part of the original D3Volume. cR and cC determine the center of copying, r and c the size of the subvolume.
 //If the size exceeds the underlying volume the submodule is filled(padded with Zeros.
-func (vol *NVolume) SubVolumePadded(cR, cC, r, c int) Volume {
+func (vol *D3Volume) SubVolumePadded(cR, cC, r, c int) Volume {
 
 	if r%2 == 0 || c%2 == 0 {
 		log.Fatal(errors.New("Even dimensions not allowed for subvolumes"))
@@ -207,12 +208,12 @@ func (vol *NVolume) SubVolumePadded(cR, cC, r, c int) Volume {
 	return sub
 }
 
-//SubVolume returns a part of the original NVolume. tR and tC determine the center of copying, r and c the size of the subvolume.
+//SubVolume returns a part of the original D3Volume. tR and tC determine the center of copying, r and c the size of the subvolume.
 //If the size exceeds the underlying volume the an error will be thrown, padding is not allowed.
-func (vol *NVolume) SubVolume(tR, tC, r, c int) Volume {
+func (vol *D3Volume) SubVolume(tR, tC, r, c int) Volume {
 
 	if tR+r > vol.Rows() || tC+c > vol.Collumns() {
-		log.Fatal(errors.New("NVolume: Subvolume size exceeds volume dimensions"))
+		log.Fatal(errors.New("D3Volume: Subvolume size exceeds volume dimensions"))
 	}
 
 	sub := New(r, c, vol.Depth())
@@ -229,27 +230,27 @@ func (vol *NVolume) SubVolume(tR, tC, r, c int) Volume {
 }
 
 //Equals compares the volume to another volume
-func (vol *NVolume) Equals(in Volume) bool {
+func (vol *D3Volume) Equals(in Volume) bool {
 	return vol.SimilarTo(in, 0)
 }
 
 //GetAt returns the element of the volume at a given position
-func (vol *NVolume) GetAt(r, c, d int) float64 {
+func (vol *D3Volume) GetAt(r, c, d int) float64 {
 	return vol.Fields[d].At(r, c)
 }
 
 //SetAt sets the element of a volume at a given position
-func (vol *NVolume) SetAt(r, c, d int, val float64) {
+func (vol *D3Volume) SetAt(r, c, d int, val float64) {
 	if r >= vol.Rows() || c >= vol.Collumns() || d >= vol.Depth() {
 		fmt.Printf("SetAt request out of bounds (RxCxD) = %vx%vx%v requested for (RxCxD) = %vx%vx%vx", r, c, d, vol.Rows(), vol.Collumns(), vol.Depth())
 		panic("out od bounds")
-		log.Fatal(errors.New("robonet.NVolume: setAt out of bounds"))
+		log.Fatal(errors.New("robonet.D3Volume: setAt out of bounds"))
 	}
 	vol.Fields[d].Set(r, c, val)
 }
 
-//Print prints the NVolume to the console in a pretty format
-func (vol *NVolume) Print() {
+//Print prints the D3Volume to the console in a pretty format
+func (vol *D3Volume) Print() {
 
 	for i := range vol.Fields {
 		fa := mat64.Formatted(&vol.Fields[i], mat64.Prefix(" "))
@@ -257,26 +258,26 @@ func (vol *NVolume) Print() {
 	}
 }
 
-// Rows of the NVolume
-func (vol *NVolume) Rows() int {
+// Rows of the D3Volume
+func (vol *D3Volume) Rows() int {
 	r, _, _ := vol.Shape()
 	return r
 }
 
-// Collumns of the NVolume
-func (vol *NVolume) Collumns() int {
+// Collumns of the D3Volume
+func (vol *D3Volume) Collumns() int {
 	_, c, _ := vol.Shape()
 	return c
 }
 
-//Depth of the NVolume
-func (vol *NVolume) Depth() int {
+//Depth of the D3Volume
+func (vol *D3Volume) Depth() int {
 	_, _, d := vol.Shape()
 	return d
 }
 
 //EqualSize checks if the size of two volumes are the same
-func (vol *NVolume) EqualSize(a Volume) bool {
+func (vol *D3Volume) EqualSize(a Volume) bool {
 	if a == nil {
 		return false
 	}
@@ -286,7 +287,7 @@ func (vol *NVolume) EqualSize(a Volume) bool {
 }
 
 //PointReflect calculates the pointreflection of a volume
-func (vol *NVolume) PointReflect() {
+func (vol *D3Volume) PointReflect() {
 	r, c, d := vol.Shape()
 	temp := New(c, r, d)
 
@@ -301,7 +302,7 @@ func (vol *NVolume) PointReflect() {
 }
 
 //Reflect calculates the reflectio of a volume (left-right)
-func (vol *NVolume) Reflect() {
+func (vol *D3Volume) Reflect() {
 
 	r, c, d := vol.Shape()
 	temp := New(r, c, d)
@@ -317,7 +318,7 @@ func (vol *NVolume) Reflect() {
 }
 
 //MulElem multiplies the volume with another volume element-wise
-func (vol *NVolume) MulElem(v1 Volume) {
+func (vol *D3Volume) MulElem(v1 Volume) {
 	r, c, d := vol.Shape()
 
 	res := New(r, c, d)
@@ -336,7 +337,7 @@ func (vol *NVolume) MulElem(v1 Volume) {
 }
 
 //Max returns the hightest number in a volume
-func (vol NVolume) Max() float64 {
+func (vol D3Volume) Max() float64 {
 	max := 0.0
 	for i := 0; i < vol.Rows(); i++ {
 		for j := 0; j < vol.Collumns(); j++ {
@@ -351,7 +352,7 @@ func (vol NVolume) Max() float64 {
 }
 
 //Min returns the lowest number in a volume
-func (vol NVolume) Min() float64 {
+func (vol D3Volume) Min() float64 {
 	min := 0.0
 	for i := 0; i < vol.Rows(); i++ {
 		for j := 0; j < vol.Collumns(); j++ {
@@ -366,7 +367,7 @@ func (vol NVolume) Min() float64 {
 }
 
 //SimilarTo compares two volumes with a given threshold
-func (vol *NVolume) SimilarTo(in Volume, threshold float64) bool {
+func (vol *D3Volume) SimilarTo(in Volume, threshold float64) bool {
 
 	if !vol.EqualSize(in) {
 		return false
@@ -388,7 +389,7 @@ func (vol *NVolume) SimilarTo(in Volume, threshold float64) bool {
 }
 
 //Elems returns the number of elements in a volume
-func (vol *NVolume) Elems() int {
+func (vol *D3Volume) Elems() int {
 	return vol.Rows() * vol.Collumns() * vol.Depth()
 
 }
